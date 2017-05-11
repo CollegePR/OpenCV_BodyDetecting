440bfa87209e43c46d5747c6f57a52559eef30dc
박정현 <pjh9996@naver.com>
2017-05-12 00:43:27 +0900

    LineData.h Edit
    Point, Line 구조체 정의

--- BodySizeMeasurement/BodySizeMeasurement/LineData.h
+++ BodySizeMeasurement/BodySizeMeasurement/LineData.h
@@ -1,1 +1,11 @@
 #pragma once
+
+struct new_point {
+	double x;
+	double y;
+};
+
+struct new_line {
+	new_point sp;
+	new_point mp;
+};
--- BodySizeMeasurement/BodySizeMeasurement/main.cpp
+++ BodySizeMeasurement/BodySizeMeasurement/main.cpp
@@ -5,5 +5,6 @@ int main()
 	Mat org_image = imread("image.jpeg");
 
 	imshow("rest", org_image);
+	waitKey(0);
 	return 0;
 }
