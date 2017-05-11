3908e718156c690b3df75a57f4f97f6e03521c9f
박정현 <pjh9996@naver.com>
2017-05-12 00:49:33 +0900

    GridRecovery.h Edit
    격자복원
    헤더파일 함수정의부 추가
    소스파일 함수구현부 추가

---
+++ BodySizeMeasurement/BodySizeMeasurement/GridRecovery.cpp
@@ -0,0 +1,19 @@
+#include "GridRecovery.h"
+
+bool isEqualPoint(Point p1, Point p2, int C)
+{
+	return false;
+}
+
+vector<Point> ContourOptimization(vector<Point>& input)
+{
+	return vector<Point>();
+}
+
+void get4SidePoints(vector<Point>& result, vector<Point>& left, vector<Point>& top, vector<Point>& right, vector<Point>& bottom)
+{
+}
+
+void GridRecovery(Mat grabcut_background_image)
+{
+}
